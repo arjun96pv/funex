c0180c755d204c0c7b64781019f45a76525e283c
arjun96pv <pvarjunguptha@gmail.com>
2024-11-14 14:33:32 +0530

    Update Function.java

--- src/main/java/azure/fun/Function.java
+++ src/main/java/azure/fun/Function.java
@@ -35,7 +35,7 @@ public class Function {
         final String name = request.getBody().orElse(query);
 
         if (name == null) {
-            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query").build();
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("hello this is new application Please pass a name on the query").build();
         } else {
             return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
         }
